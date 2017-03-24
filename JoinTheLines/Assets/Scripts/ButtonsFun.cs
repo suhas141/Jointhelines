c2807dae2ee087ed019e1d69861bf91eb547aba3
suhas141 <murthy.su@husky.neu.edu>
2017-03-23 22:51:25 -0400

    all the files added

---
+++ JoinTheLines/Assets/Scripts/ButtonsFun.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsFun : MonoBehaviour
+{
+    public void ChangeScene(string SceneName)
+    {
+        Application.LoadLevel(SceneName);
+    }
+}
---
+++ JoinTheLines/Assets/Scripts/FallScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallScript : MonoBehaviour {
+    public GameObject Plane;
+    public GameObject SpawnPoint;
+
+    void Start()
+    {
+
+    }
+
+    void Update ()
+    {
+        if(transform.position.y < Plane.transform.position.y - 10)
+        {
+            transform.position = SpawnPoint.transform.position;
+        }
+    }	
+}
